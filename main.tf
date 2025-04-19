f20416e0d928a3bc8247b81dca762f59f09edc81
evan <evanjj@protonmail.com>
2025-04-19 21:33:36 +0000

    syntax

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
   features {
   }
   skip_provider_registration = true
-  subscription_id = "28e1e42a-4438-4c30-9a5f-7d7b488fd883"
+  subscription_id            = "28e1e42a-4438-4c30-9a5f-7d7b488fd883"
 }
 
 resource "azurerm_resource_group" "rg" {
